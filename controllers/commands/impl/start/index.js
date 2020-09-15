d492213402591f317d9e94519acceb408fe8a739
Liza4239 <Elyzaveta13fsn@gmail.com>
2020-09-15 21:51:53 +0300

    Update index.js

--- controllers/commands/impl/start/index.js
+++ controllers/commands/impl/start/index.js
@@ -7,33 +7,40 @@ const conf = require('./config');
 const {
     visitor
 } = require('../../../../configs/google/analytics');
-const{ msgStart }= require('./config');
+const { msgStart } = require('./config');
 
 bot.command('start',
     ctx => {
         ctx.reply(
             msgStart,
             Extra.HTML().markup((m) =>
-            m.inlineKeyboard([
+                [
+                    [
 
-                m.inlineKeyboard([ 
-                m.callbackButton('Our programmes', 'ActionHandlerOurProgrammes'),
-                m.callbackButton("Roles", "ActionHandlerRoles")
-            ])
-        ]),
-            
 
-                m.inlineKeyboard([ 
-                m.inlineKeyboard([
+                        m.inlineKeyboard([
+                            m.callbackButton('Our programmes', 'ActionHandlerOurProgrammes'),
+                            m.callbackButton("Roles", "ActionHandlerRoles")
+                        ])
+                    ]
+                ],
 
-                    m.callbackButton("Job responsibilities", "ActionHandlerJobResponsibilities"),
-                    m.callbackButton("Basic qualifications", "ActionHandlerBasicQualifications"),
-                    m.callbackButton("Useful links", "ActionHandlerUsefulLinks")])
-            ])
-                    
-                    
-                
-            
+                [
+                  [
+                    m.inlineKeyboard([
+                        m.inlineKeyboard([
+    
+                            m.callbackButton("Job responsibilities", "ActionHandlerJobResponsibilities"),
+                            m.callbackButton("Basic qualifications", "ActionHandlerBasicQualifications"),
+                            m.callbackButton("Useful links", "ActionHandlerUsefulLinks")])
+                    ])
+                  ]  
+                ]
+               
+
+
+
+
             )
         )
     }
